d6316dc5962dc2b2edd8876f5af06a021df62d1d
ievgen.kliuchyk <kliuchyk@gmail.com>
2023-12-08 17:34:30 +0100

    run base server config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello TypeScript!');
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
